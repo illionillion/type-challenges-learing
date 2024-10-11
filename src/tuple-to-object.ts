bd5ac7f9ad99a3cd3654cc90201d6e4bd24d5cb8
illionillion <60034520+illionillion@users.noreply.github.com>
2024-10-11 02:30:01 +0000

    タプル型をオブジェクト型に変換

---
+++ src/tuple-to-object.ts
@@ -0,0 +1,10 @@
+// Tuple型からObject型を生成するTupleToObjectを実装
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  // T[number]にあるやつをKとする
+  [K in T[number]]: K;
+};
+
+type result = TupleToObject<typeof tuple>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
