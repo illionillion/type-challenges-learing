85d3a4ee5c0068f2516f9ebbe6c853753ec5d1cd
yuto ichinose <yuto.0526.sano@gmail.com>
2025-02-02 12:04:05 +0900

    fix: 例追加

--- src/lookup.ts
+++ src/lookup.ts
@@ -14,3 +14,16 @@ type LookUp<T, U> = T extends { type: U } ? T : never;
 type MyDog = LookUp<Cat | Dog, "dog">; // expected to be `Dog`
 type MyCat = LookUp<Cat | Dog, "cat">; // expected to be `Cat`
 type MyNever = LookUp<Cat | Dog, "cataa">; // expected to be `never`
+
+let dog: MyDog = {
+    type: "dog",
+    breeds: "Boxer",
+    color: "brown"
+}
+
+// never型じゃないから再代入できる
+dog = {
+    breeds: "Bulldog",
+    color: "white",
+    type: "dog"
+}
