9e616ad2b7731666b124480cfc079e2e3157b637
yuto ichinose <yuto.0526.sano@gmail.com>
2025-02-02 11:49:32 +0900

    feat: 文字列数の吸い出し追加

--- src/array-length-type.ts
+++ src/array-length-type.ts
@@ -24,3 +24,16 @@ const array = [
 // as constつけないとnumber[]型になる
 
 type arrayLength = Length<typeof array> // 3
+
+// 文字列から総数を吸い出すには一度タプルにする必要がある
+// 再帰的にタプル型を作り、長さを計算
+type StringToTuple<T extends string, A extends any[] = []> = 
+  T extends `${infer First}${infer Rest}` 
+    ? StringToTuple<Rest, [First, ...A]> 
+    : A;
+
+type StringLength<T extends string> = StringToTuple<T>["length"];
+
+type HogeLength = StringLength<"hoge">; // 4
+type EmptyLength = StringLength<"">;    // 0
+type LongLength = StringLength<"typescript">; // 10
