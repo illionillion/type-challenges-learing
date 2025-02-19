0b42ce03ebec64e5436de219318e390f8362c4c7
yuto ichinose <yuto.0526.sano@gmail.com>
2025-02-19 14:26:12 +0900

    fix: Pop型の実装に関するコメントを修正

--- src/pop.ts
+++ src/pop.ts
@@ -1,7 +1,8 @@
 // Pop型を実装してください。
 // Pop型は、配列の最後の要素を削除した配列を返す型です。
-// type Pop<T extends any[]> = T extends [...infer Rest, any] ? Rest : never;
-type Pop<T> = T extends [...infer U, infer _] ? U : never;
+// any[]で配列の型か確かめるのが正しい
+type Pop<T extends any[]> = T extends [...infer Rest, any] ? Rest : never;
+// type Pop<T> = T extends [...infer U, infer _] ? U : never;
 
 type arr1 = ["a", "b", "c", "d"];
 type arr2 = [3, 2, 1];
